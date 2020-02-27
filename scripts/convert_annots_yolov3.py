06fb39ef5e635a6ae627cabb403480b3ce27f0b5
dhruv <dhruv.mohan@outlook.com>
2020-02-27 14:50:04 +0530

    update config

--- scripts/convert_annots_yolov3.py
+++ scripts/convert_annots_yolov3.py
@@ -4,15 +4,34 @@ _config_path_ = '../project_config/yolov3.config'
 cfg = config()
 cfg.load_data(_config_path_)
 
+with open(cfg.data["Dataset_file"], "w") as file:
+  pass
+
 def process_lines(image_path, lines):
   final_line = image_path[:-4] + '.jpg '
   for line in lines:
-    data = line.split()
-    for i in range(1,len(data)):
-      final_line += data[i] +','
-    final_line += data[0] + ' '
-    print(final_line)
-    input(data)
+    c, x, y, w, h = line.split()
+    w1 = float(w)/2 + float(x)
+    h1 = float(h)/2 + float(y)
+    print(x, y, w1 ,h1)
+    x = float(x) * 1920
+    y = float(y) * 1080
+    w = float(w) * 1920
+    h = float(h) * 1080
+    x = x - w/2
+    y = y - h/2
+
+    w = x + w
+    h = y + h
+
+    final_line += str(x) + ', ' + str(y) + ', ' + str(w) + ', ' + str(h) + ', ' + c + ' '
+    #final_line += c + ' '
+    #input(final_line)
+
+  final_line += '\n'
+
+  with open(cfg.data["Dataset_file"], "a+") as file:
+    file.write(final_line)
 
 def get_file_path(file_name):
   file_path = os.path.join(cfg.data["Data_Path"], file_name)
@@ -23,7 +42,7 @@ def read_file(file_name):
   with open(file_path, 'r') as file:
     lines = file.readlines()
     process_lines(file_path, lines)
-    input(lines)
+    #input(lines)
   pass
 
 def is_file(name):
--- scripts/create_config.py
+++ scripts/create_config.py
@@ -5,5 +5,5 @@ _config_path = '../project_config/yolov3.config'
 cfg= {}
 cfg["Data_Path"] = '/media/dhruv/Blue22/Blue2/downloads/yolov3/total'
 cfg["Base_Dir"] = '/media/dhruv/Blue22/Blue2/downloads/yolov3/'
-cfg['Dataset_file']
+cfg['Dataset_file'] = '/media/dhruv/Blue22/Blue2/downloads/yolov3/output/dataset.txt'
 write_json(_config_path, cfg)
