48ce921ca7bf5ad89048bc8290ca340e5112c7ca
Dhruv-Mohan <dhruv.mohan@outlook.com>
2020-02-27 16:16:44 +0530

    integrating config into core

--- scripts/create_config.py
+++ scripts/create_config.py
@@ -3,7 +3,8 @@ from generic_modules.FileIO import *
 _config_path = '../project_config/yolov3.config'
 
 cfg= {}
-cfg["Data_Path"] = '/media/dhruv/Blue22/Blue2/downloads/yolov3/total'
-cfg["Base_Dir"] = '/media/dhruv/Blue22/Blue2/downloads/yolov3/'
-cfg['Dataset_file'] = '/media/dhruv/Blue22/Blue2/downloads/yolov3/output/dataset.txt'
+cfg["Data_Path"] = 'C:\\Users\\Dhruv\\Documents\\Dataset\\yolo_test\\2\\2\\'
+cfg["Base_Dir"] = 'C:\\Users\\Dhruv\\Documents\\Dataset\\yolo_test\\'
+cfg['Dataset_file'] = 'C:\\Users\\Dhruv\\Documents\\Dataset\\yolo_test\\input\\dataset.txt'
+cfg['Classes'] = 'C:\\Users\\Dhruv\\Documents\\Dataset\\yolo_test\\input\\classes.names'
 write_json(_config_path, cfg)
